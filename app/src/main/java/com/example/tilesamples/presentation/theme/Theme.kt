4d3ca5176951e3ac6b94f32fb395c1da48944027
nosho.hiroaki <nosho404@gmail.com>
2024-07-06 08:48:47 +0900

    first_commit

---
+++ app/src/main/java/com/example/tilesamples/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.tilesamples.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun TileSamplesTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
