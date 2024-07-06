e89a6d1f7bda4ac85289f21e7ec16e71d14f3dc5
nosho.hiroaki <nosho404@gmail.com>
2024-07-06 09:20:05 +0900

    implements DynamicExpressionTileService

---
+++ app/src/main/java/com/example/tilesamples/dynamic_expression_tile_service/DynamicExpressionTileService.kt
@@ -0,0 +1,79 @@
+package com.example.tilesamples.dynamic_expression_tile_service
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.protolayout.ColorBuilders.argb
+import androidx.wear.protolayout.LayoutElementBuilders
+import androidx.wear.protolayout.ResourceBuilders
+import androidx.wear.protolayout.TimelineBuilders
+import androidx.wear.protolayout.TypeBuilders
+import androidx.wear.protolayout.expression.DynamicBuilders.DynamicInstant
+import androidx.wear.protolayout.material.Colors
+import androidx.wear.protolayout.material.Text
+import androidx.wear.protolayout.material.Typography
+import androidx.wear.protolayout.material.layouts.PrimaryLayout
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.TileBuilders
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.compose.tools.LayoutRootPreview
+import com.google.android.horologist.compose.tools.buildDeviceParameters
+import com.google.android.horologist.tiles.SuspendingTileService
+import java.time.Instant
+
+private const val RESOURCES_VERSION = "0"
+
+@OptIn(ExperimentalHorologistApi::class)
+class DynamicExpressionTileService : SuspendingTileService() {
+
+    override suspend fun resourcesRequest(
+        requestParams: RequestBuilders.ResourcesRequest
+    ): ResourceBuilders.Resources {
+        return ResourceBuilders.Resources.Builder().setVersion(RESOURCES_VERSION).build()
+    }
+
+    override suspend fun tileRequest(
+        requestParams: RequestBuilders.TileRequest
+    ): TileBuilders.Tile {
+        val singleTileTimeline = TimelineBuilders.Timeline.Builder().addTimelineEntry(
+            TimelineBuilders.TimelineEntry.Builder().setLayout(
+                LayoutElementBuilders.Layout.Builder().setRoot(tileLayout(this)).build()
+            ).build()
+        ).build()
+
+        return TileBuilders.Tile.Builder().setResourcesVersion(RESOURCES_VERSION)
+            .setTileTimeline(singleTileTimeline).build()
+    }
+}
+
+private fun tileLayout(context: Context): LayoutElementBuilders.LayoutElement {
+    val time = DynamicInstant.withSecondsPrecision(Instant.EPOCH)
+        .durationUntil(DynamicInstant.platformTimeWithSecondsPrecision())
+        .secondsPart
+        .asFloat()
+
+    return PrimaryLayout.Builder(buildDeviceParameters(context.resources))
+        .setContent(
+            Text.Builder(
+                context,
+                TypeBuilders.StringProp.Builder("00")
+                    .setDynamicValue(time.format())
+                    .build(),
+                TypeBuilders.StringLayoutConstraint.Builder("00")
+                    .build()
+            ).build()
+        ).build()
+}
+
+@Preview(
+    device = Devices.WEAR_OS_SMALL_ROUND,
+    showSystemUi = true,
+    backgroundColor = 0xff000000,
+    showBackground = true
+)
+@Composable
+fun TilePreview() {
+    LayoutRootPreview(root = tileLayout(LocalContext.current))
+}
