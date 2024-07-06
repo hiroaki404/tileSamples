8034279ecbab0bd5738762bdddd00f4f7266794d
nosho.hiroaki <48251111+hiroaki404@users.noreply.github.com>
2024-07-06 16:07:31 +0900

    add UseRendererWithImageResourceTileService

---
+++ app/src/main/java/com/example/tilesamples/tiles/use_renderer_with_image_resource/UseRendererWithImageResourceTileService.kt
@@ -0,0 +1,32 @@
+package com.example.tilesamples.tiles.use_renderer_with_image_resource
+
+import androidx.wear.protolayout.ResourceBuilders
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.TileBuilders
+import com.example.tilesamples.tiles.use_minimum_renderer.SimpleTileRenderer
+import com.example.tilesamples.tiles.use_minimum_renderer.SimpleTileState
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.tiles.SuspendingTileService
+
+@OptIn(ExperimentalHorologistApi::class)
+class UseRendererWithImageResourceTileService: SuspendingTileService() {
+    private lateinit var renderer: SimpleTileRenderer
+
+    override fun onCreate() {
+        super.onCreate()
+        renderer = SimpleTileRenderer(this)
+    }
+
+    override suspend fun resourcesRequest(
+        requestParams: RequestBuilders.ResourcesRequest
+    ): ResourceBuilders.Resources {
+        return renderer.produceRequestedResources(Unit, requestParams)
+    }
+
+    override suspend fun tileRequest(
+        requestParams: RequestBuilders.TileRequest
+    ): TileBuilders.Tile {
+        val state = SimpleTileState("hello world")
+        return renderer.renderTimeline(state, requestParams)
+    }
+}
