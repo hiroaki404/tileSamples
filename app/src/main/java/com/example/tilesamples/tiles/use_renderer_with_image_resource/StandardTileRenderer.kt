1fe343ca70157296bfb7b07fd9fabe1b1516c556
nosho.hiroaki <48251111+hiroaki404@users.noreply.github.com>
2024-07-06 17:38:10 +0900

    give coil image resource

--- app/src/main/java/com/example/tilesamples/tiles/use_renderer_with_image_resource/StandardTileRenderer.kt
+++ app/src/main/java/com/example/tilesamples/tiles/use_renderer_with_image_resource/StandardTileRenderer.kt
@@ -1,9 +1,13 @@
 package com.example.tilesamples.tiles.use_renderer_with_image_resource
 
 import android.content.Context
+import android.graphics.Bitmap
 import androidx.wear.protolayout.ColorBuilders.argb
 import androidx.wear.protolayout.DeviceParametersBuilders.DeviceParameters
 import androidx.wear.protolayout.LayoutElementBuilders
+import androidx.wear.protolayout.ResourceBuilders
+import androidx.wear.protolayout.ResourceBuilders.ImageResource
+import androidx.wear.protolayout.ResourceBuilders.Resources
 import androidx.wear.protolayout.material.Colors
 import androidx.wear.protolayout.material.Text
 import androidx.wear.protolayout.material.Typography
@@ -12,13 +16,15 @@ import androidx.wear.tiles.tooling.preview.Preview
 import androidx.wear.tiles.tooling.preview.TilePreviewData
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.android.horologist.tiles.render.SingleTileLayoutRenderer
+import java.nio.ByteBuffer
 
 data class SimpleTileState(
     val text: String
 )
 
 @OptIn(ExperimentalHorologistApi::class)
-class StandardTileRenderer(context: Context): SingleTileLayoutRenderer<SimpleTileState, Unit>(context) {
+class StandardTileRenderer(context: Context) :
+    SingleTileLayoutRenderer<SimpleTileState, Bitmap?>(context) {
     override fun renderTile(
         state: SimpleTileState,
         deviceParameters: DeviceParameters
@@ -26,7 +32,10 @@ class StandardTileRenderer(context: Context): SingleTileLayoutRenderer<SimpleTil
         return tileLayout(state, deviceParameters)
     }
 
-    private fun tileLayout(state: SimpleTileState, deviceParameters: DeviceParameters): LayoutElementBuilders.LayoutElement {
+    private fun tileLayout(
+        state: SimpleTileState,
+        deviceParameters: DeviceParameters
+    ): LayoutElementBuilders.LayoutElement {
         return PrimaryLayout.Builder(deviceParameters)
             .setContent(
                 Text.Builder(context, state.text)
@@ -35,6 +44,34 @@ class StandardTileRenderer(context: Context): SingleTileLayoutRenderer<SimpleTil
                     .build()
             ).build()
     }
+
+    override fun Resources.Builder.produceRequestedResources(
+        resourceState: Bitmap?,
+        deviceParameters: DeviceParameters,
+        resourceIds: List<String>
+    ) {
+        resourceState?.let {
+            addIdToImageMapping("id", bitmapToImageResource(it))
+        }
+    }
+}
+
+fun bitmapToImageResource(bitmap: Bitmap): ImageResource {
+    val safeBitmap = bitmap.copy(Bitmap.Config.RGB_565, false)
+
+    val byteBuffer = ByteBuffer.allocate(safeBitmap.byteCount)
+    safeBitmap.copyPixelsToBuffer(byteBuffer)
+    val bytes: ByteArray = byteBuffer.array()
+
+    return ImageResource.Builder().setInlineResource(
+        ResourceBuilders.InlineImageResource.Builder()
+            .setData(bytes)
+            .setWidthPx(bitmap.width)
+            .setHeightPx(bitmap.height)
+            .setFormat(ResourceBuilders.IMAGE_FORMAT_RGB_565)
+            .build()
+    )
+        .build()
 }
 
 @Preview
--- app/src/main/java/com/example/tilesamples/tiles/use_renderer_with_image_resource/UseRendererWithImageResourceTileService.kt
+++ app/src/main/java/com/example/tilesamples/tiles/use_renderer_with_image_resource/UseRendererWithImageResourceTileService.kt
@@ -1,10 +1,17 @@
 package com.example.tilesamples.tiles.use_renderer_with_image_resource
 
+import android.graphics.drawable.BitmapDrawable
 import androidx.wear.protolayout.ResourceBuilders
 import androidx.wear.tiles.RequestBuilders
 import androidx.wear.tiles.TileBuilders
+import coil.Coil
+import coil.request.ImageRequest
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.android.horologist.tiles.SuspendingTileService
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+
+const val imageUrl = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhoo1K7MPUKW-dzDxsRKYw-9ZUgu6PCs7u-4v9Mj8gMau7N1ewfC16IusZT9U-qBnTR2u_rI_BCLEc9-sVV0M32e-ABz2dPN2tKBMu26ZbAr5dahgTmCvlz5LwpGrU4SSuID6Ns7k6kQytZ/s800/bird_maruitori.png"
 
 @OptIn(ExperimentalHorologistApi::class)
 class UseRendererWithImageResourceTileService: SuspendingTileService() {
@@ -18,7 +25,19 @@ class UseRendererWithImageResourceTileService: SuspendingTileService() {
     override suspend fun resourcesRequest(
         requestParams: RequestBuilders.ResourcesRequest
     ): ResourceBuilders.Resources {
-        return renderer.produceRequestedResources(Unit, requestParams)
+        val image = coroutineScope {
+            async {
+                Coil.imageLoader(this@UseRendererWithImageResourceTileService).run {
+                    val request = ImageRequest.Builder(this@UseRendererWithImageResourceTileService)
+                        .data(imageUrl)
+                        .build()
+                    val result = execute(request)
+                    (result.drawable as? BitmapDrawable)?.bitmap
+                }
+            }
+        }.await()
+
+        return renderer.produceRequestedResources(image, requestParams)
     }
 
     override suspend fun tileRequest(
