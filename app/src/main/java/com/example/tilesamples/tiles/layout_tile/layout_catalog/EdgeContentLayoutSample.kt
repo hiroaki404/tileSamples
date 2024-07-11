a9cd3b28d308cf5037aa60fc3417eb6fb8efe6dd
nosho.hiroaki <nosho404@gmail.com>
2024-07-11 09:57:24 +0900

    add click action

--- app/src/main/java/com/example/tilesamples/tiles/layout_tile/layout_catalog/EdgeContentLayoutSample.kt
+++ app/src/main/java/com/example/tilesamples/tiles/layout_tile/layout_catalog/EdgeContentLayoutSample.kt
@@ -1,9 +1,13 @@
 package com.example.tilesamples.tiles.layout_tile.layout_catalog
 
 import android.content.Context
+import androidx.wear.protolayout.ActionBuilders
 import androidx.wear.protolayout.ColorBuilders.argb
 import androidx.wear.protolayout.DeviceParametersBuilders.DeviceParameters
+import androidx.wear.protolayout.DimensionBuilders
+import androidx.wear.protolayout.LayoutElementBuilders
 import androidx.wear.protolayout.LayoutElementBuilders.LayoutElement
+import androidx.wear.protolayout.ModifiersBuilders
 import androidx.wear.protolayout.material.CircularProgressIndicator
 import androidx.wear.protolayout.material.Colors
 import androidx.wear.protolayout.material.ProgressIndicatorColors
@@ -12,40 +16,60 @@ import androidx.wear.protolayout.material.Typography
 import androidx.wear.protolayout.material.layouts.EdgeContentLayout
 import androidx.wear.tiles.tooling.preview.Preview
 import androidx.wear.tiles.tooling.preview.TilePreviewData
+import com.example.tilesamples.tiles.layout_tile.SIMPLE_LAYOUT
 import com.example.tilesamples.tiles.layout_tile.createTile
 
 val edgeContentLayout: (Context, DeviceParameters) -> LayoutElement = { context, deviceParameters ->
-    EdgeContentLayout.Builder(deviceParameters)
-        .setEdgeContent(
-            CircularProgressIndicator.Builder()
-                .setProgress(0.5f)
-                .setEndAngle(270f)
-                .setCircularProgressIndicatorColors(ProgressIndicatorColors.progressIndicatorColors(Colors.DEFAULT))
-                .setStrokeWidth(12f)
+    LayoutElementBuilders.Box.Builder()
+        .setModifiers(
+            ModifiersBuilders.Modifiers.Builder()
+                .setClickable(
+                    ModifiersBuilders.Clickable.Builder()
+                        .setId(SIMPLE_LAYOUT)
+                        .setOnClick(ActionBuilders.LoadAction.Builder().build())
+                        .build()
+                )
                 .build()
         )
-        .setContent(
-            Text.Builder(context, "Content")
-                .setColor(argb(Colors.DEFAULT.onSurface))
-                .setTypography(Typography.TYPOGRAPHY_CAPTION1)
-                .build()
-        )
-        .setPrimaryLabelTextContent(
-            Text.Builder(context, "Primary Label Text")
-                .setColor(argb(Colors.DEFAULT.onSurface))
-                .setTypography(Typography.TYPOGRAPHY_CAPTION1)
-                .build()
+        .setHeight(DimensionBuilders.expand())
+        .setWidth(DimensionBuilders.expand())
+        .addContent(
+            EdgeContentLayout.Builder(deviceParameters)
+                .setEdgeContent(
+                    CircularProgressIndicator.Builder()
+                        .setProgress(0.5f)
+                        .setEndAngle(270f)
+                        .setCircularProgressIndicatorColors(
+                            ProgressIndicatorColors.progressIndicatorColors(
+                                Colors.DEFAULT
+                            )
+                        )
+                        .setStrokeWidth(12f)
+                        .build()
+                )
+                .setContent(
+                    Text.Builder(context, "Content")
+                        .setColor(argb(Colors.DEFAULT.onSurface))
+                        .setTypography(Typography.TYPOGRAPHY_CAPTION1)
+                        .build()
+                )
+                .setPrimaryLabelTextContent(
+                    Text.Builder(context, "Primary Label Text")
+                        .setColor(argb(Colors.DEFAULT.onSurface))
+                        .setTypography(Typography.TYPOGRAPHY_CAPTION1)
+                        .build()
 
-        )
-        .setSecondaryLabelTextContent(
-            Text.Builder(context, "Secondary Label Text")
-                .setColor(argb(Colors.DEFAULT.onSurface))
-                .setTypography(Typography.TYPOGRAPHY_CAPTION1)
+                )
+                .setSecondaryLabelTextContent(
+                    Text.Builder(context, "Secondary Label Text")
+                        .setColor(argb(Colors.DEFAULT.onSurface))
+                        .setTypography(Typography.TYPOGRAPHY_CAPTION1)
+                        .build()
+
+
+                )
                 .build()
-
-
-        )
-        .build()
+        ).build()
 }
 
 @Preview
